bf89d5263c199df0a4b19a57a67320f3f18041d7
ThisisYoYoDev <yoel.edery@epitech.eu>
2022-04-30 17:51:08 +0200

    Fiw: Discord send message on event

--- back/commands/formatResponse.py
+++ back/commands/formatResponse.py
@@ -5,11 +5,11 @@ from botConfig import CHANNEL_ID
 from logger.logger import logger
 
 
-def parseData(data: dict) -> str:
+def parseData(data: dict):
     return "data"
 
-async def sendMessage(data: str):
-    channel = bot.get_channel(CHANNEL_ID)
+async def sendMessage(data: dict):
+    channel = bot.get_channel(int(CHANNEL_ID))
     if len(data) > 4000:
         logger.panic(f"Message too long: {data}")
     await channel.send(data)
--- back/main.py
+++ back/main.py
@@ -9,19 +9,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from botClient import bot
-from botConfig import TOKEN
+from botConfig import TOKEN, CHANNEL_ID
+
+from commands.formatResponse import sendMessage
 
 @app.post("/webhook")
 async def webhooks(data: dict):
     logger.info(data)
-    await send_message(data)
+    await sendMessage(data)
     return {"status": "ok"}
 
 @app.on_event("startup")
 async def startupDiscord():
     asyncio.create_task(bot.start(TOKEN))
     await asyncio.sleep(2)
-    logger.info(f"Connected as {bot.user}")
+    logger.info(f"Connected as {bot.user} !")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8080)
